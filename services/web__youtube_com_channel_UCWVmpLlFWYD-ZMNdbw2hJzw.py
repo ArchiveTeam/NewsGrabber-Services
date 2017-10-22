eee179042fb1060b0af3360d9ab3ffb0232ec11d
HarryC145 <HarryC145@users.noreply.github.com>
2017-10-23 00:00:00 +0100

    remove dodgy site that crashes the bot.. for now

--- services/web__youtube_com_channel_UCWVmpLlFWYD-ZMNdbw2hJzw.py
+++
@@ -1,7 +0,0 @@
-refresh = 43200
-version = 20161106.01
-
-urls = ['https://www.youtube.com/channel/UCWVmpLlFWYD-ZMNdbw2hJzw/videos']
-regex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
-videoregex = [r'^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=']
-liveregex = []
