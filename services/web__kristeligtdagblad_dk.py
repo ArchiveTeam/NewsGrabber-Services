1ca9e62d9722af44f0ce0e25c0d55c1acb9a470f
Lucas Rolff <lucas@lucasrolff.com>
2017-10-28 18:02:15 +0200

    Add kristeligt-dagblad.dk
    Add kristeligt-dagblad.dk news site

---
+++ services/web__kristeligtdagblad_dk.py
@@ -0,0 +1,13 @@
+refresh = 300
+version = 20161106.01
+
+urls = ['https://www.kristeligt-dagblad.dk/',
+        'https://www.kristeligt-dagblad.dk/danmark',
+        'https://www.kristeligt-dagblad.dk/liv',
+        'https://www.kristeligt-dagblad.dk/kirke',
+        'https://www.kristeligt-dagblad.dk/kultur',
+        'https://www.kristeligt-dagblad.dk/udland',
+        'https://www.kristeligt-dagblad.dk/debat']
+regex = [r'^https?:\/\/[^\/]*kristeligt-dagblad\.dk']
+videoregex = []
+liveregex = []
