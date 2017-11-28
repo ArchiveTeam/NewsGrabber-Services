1d6434c9a5b9ab54b82c5970429fe9e8ade53ae6
Brayden <CoolCanuck@users.noreply.github.com>
2017-11-27 20:27:19 -0500

    Create web__halifaxtoday_ca.py

---
+++ services/web__halifaxtoday_ca.py
@@ -0,0 +1,9 @@
+refresh = 1800
+version = 20171127.01
+
+urls = ['https://www.halifaxtoday.ca/',
+	'http://15minut.org/feed']
+regex = [r'^https?:\/\/[^\/]*halifaxtoday\.ca\/']	
+videoregex = []
+liveregex = []
+
