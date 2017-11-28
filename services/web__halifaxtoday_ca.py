1d8d30a28fc9007984978cc124df6310553b745e
Brayden <CoolCanuck@users.noreply.github.com>
2017-11-27 20:29:20 -0500

    Update web__halifaxtoday_ca.py

--- services/web__halifaxtoday_ca.py
+++ services/web__halifaxtoday_ca.py
@@ -1,8 +1,7 @@
 refresh = 1800
 version = 20171127.01
 
-urls = ['https://www.halifaxtoday.ca/',
-	'http://15minut.org/feed']
+urls = ['https://www.halifaxtoday.ca/']
 regex = [r'^https?:\/\/[^\/]*halifaxtoday\.ca\/']	
 videoregex = []
 liveregex = []
