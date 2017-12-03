738060b3cd499d1ec2a6e40a7148e888db3d3497
Brayden <CoolCanuck@users.noreply.github.com>
2017-12-03 12:37:06 -0500

    Create web__stthomastimesjournal_com.py

---
+++ services/web__stthomastimesjournal_com.py
@@ -0,0 +1,7 @@
+refresh = 86400
+version = 20171203.01
+
+urls = ['http://www.stthomastimesjournal.com/']
+regex = [r'^https?:\/\/[^\/]*stthomastimesjournal.com\/']	
+videoregex = []
+liveregex = []
