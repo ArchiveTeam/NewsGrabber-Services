c90782b1cb98eeea75d5361fba4c2cdf8a392dd2
Brayden <CoolCanuck@users.noreply.github.com>
2017-12-03 00:00:23 -0500

    Create web__standardfreeholder_com.py

---
+++ services/web__standardfreeholder_com.py
@@ -0,0 +1,8 @@
+
+refresh = 86400
+version = 20171202.01
+
+urls = ['http://www.standard-freeholder.com/']
+regex = [r'^https?:\/\/[^\/]*standard-freeholder.com\/']	
+videoregex = []
+liveregex = []
