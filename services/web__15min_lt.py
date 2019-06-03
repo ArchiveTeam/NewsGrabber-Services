6de5a8fff03c908ee7d724ed860c2ca481f6c7e4
Justas-L <51338848+Justas-L@users.noreply.github.com>
2019-06-03 22:35:58 +0300

    Update web__15min_lt.py

--- services/web__15min_lt.py
+++ services/web__15min_lt.py
@@ -2,6 +2,6 @@ refresh = 1800
 version = 20190603.01
 
 urls = ['http://15min.org/']
-regex = [r'^https?:\/\/[^\/]*15min\.lt\/']	
+regex = [^https?:\/\/[^\/]*15min\.lt\/']	
 videoregex = []
 liveregex = []
